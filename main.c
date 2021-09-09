7d0034cccbc9bcf3c3ca98a077b42664b192d3ad
Ivo Baptista <ivobass@gmail.com>
2021-09-09 13:19:03 +0100

    Testando Variaveis em C

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+// livreria de entrada e saida é obrigatorio para um programa basico em C
+#include <locale.h>
+
+void main() {
+    //permite utilizar acentos
+    setlocale(LC_ALL,"");
+    // Imprime ola
+    printf("óla\n");
+
+    // trabalhando com variaveis inteiras
+
+    int a = 50;
+    printf("O valor de a é %d \n",a);
+    scanf("%d", &a);
+    printf("O valor de a mudou para %d \n" ,a);
+
+    // trabalhando com variaveis fracionadas
+    float b = 5.5;
+    printf("O valor de b é %f \n",b);
+    scanf("%f", &b);
+    printf("O valor de a mudou para %f \n" ,b);
+    // trabalhando com caracteres
+    char c = 't';
+    printf("O valor de c é %c \n",c);
+    //limpar o buffer da entrada principal
+    fflush(stdin);
+    scanf("%c", &c);
+    printf("O valor de c mudou para %c \n" ,c);
+
+}
